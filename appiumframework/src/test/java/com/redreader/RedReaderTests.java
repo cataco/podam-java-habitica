16d39393935606c9ce181f720f5e6c0cf6211ecd
ccordob <catalina.cordoba@morningstar.com>
2020-10-02 22:26:20 -0500

    first test appium

--- appiumframework/src/test/java/com/redreader/RedReaderTests.java
+++ appiumframework/src/test/java/com/redreader/RedReaderTests.java
@@ -1,19 +1,20 @@
 package com.redreader;
 //package <set your test package>;
-import static org.testng.Assert.assertTrue;
 
-import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
-//import org.junit.jupiter.api.*;
-import java.net.URL;
 import java.net.MalformedURLException;
 import java.util.logging.Level;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+//import org.junit.jupiter.api.*;
 
 public class RedReaderTests {
 
@@ -30,16 +31,32 @@ public class RedReaderTests {
     dc.setCapability("reportFormat", reportFormat);
     dc.setCapability("testName", testName);
     dc.setCapability(MobileCapabilityType.UDID, "emulator-5554");
-    dc.setCapability(MobileCapabilityType.APP, "/Users/ccordob/appiumstudio/original-apks/org.quantumbadger.redreader.activities.MainActivity.2.apk");
+    dc.setCapability(MobileCapabilityType.APP, "/Users/ccordob/Downloads/RedReader-limpia.apk");
     dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "org.quantumbadger.redreader");
     dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".activities.MainActivity");
-    driver = new AndroidDriver<>(new URL("http://localhost:4723/wd/hub"), dc);
+    driver = new AndroidDriver<>(dc);
     driver.setLogLevel(Level.INFO);
   }
 
   @Test
-  public void testUntitled() {
-    //assertTrue();
+  public void validateAccountsModalIsDisplayed() throws InterruptedException {
+    driver.findElement(By.id("android:id/button2")).click();
+    driver.findElementByAccessibilityId("More options").click();
+    driver.findElement(By.xpath("//*[@text='Accounts']")).click();
+    driver.findElements(By.id("org.quantumbadger.redreader:id/recycler_item_text"))
+        .forEach(element -> {
+          Assert.assertTrue(element.isDisplayed());
+        });
+    /*driver.findElement(By.xpath("//*[@text='CLOSE']")).click();
+    driver.findElement(By.xpath("//*[@contentDescription='More options']")).click();
+    driver.findElement(By.xpath("//*[@text='Themes']")).click();
+    driver.findElement(By.xpath("//*[@text='Blue']")).click();
+    driver.findElement(By.xpath("//*[@text='Front Page']")).click();
+    driver.findElement(By.xpath("//*[@text='Front Page' and ./parent::*[@class='android.widget.LinearLayout']]")).click();
+    driver.findElement(By.xpath("//*[@id='actionbar_title_back_image']")).click();
+    driver.findElement(By.xpath("//*[@text='All Subreddits']")).click();
+    driver.findElement(By.xpath("//*[@text='All Subreddits' and ./parent::*[@class='android.widget.LinearLayout']]")).click();
+    driver.findElement(By.xpath("//*[@id='actionbar_title_back_image']")).click();*/
   }
 
   @AfterTest
